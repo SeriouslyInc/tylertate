0ac1f708e60fd84fae0df8e8c5605055b3588713
Tyler Tate <ttate@nutshell.com>
2011-05-09 11:54:24 +0100

    Debugging js

--- resources/js/filter.js
+++ resources/js/filter.js
@@ -5,6 +5,10 @@ $(document).ready(function() {
 		var publisher = $(this).attr('data-publisher');
 		publishers.push(publisher);
 	});
+
+	for (var i=0; i<publishers.length; i++) {
+		$("#writing aside ul#publisher").append('<li>'+ publishers[i] +'</li>');
+	}
 	
 	console.log(publishers);
 });
