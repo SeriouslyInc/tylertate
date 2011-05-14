8d8be3b53bc9e727db4cc2e7800cef0f7646eda5
Tyler Tate <ttate@nutshell.com>
2011-05-14 11:33:47 +0100

    Giving quicksand a try

--- resources/js/filter.js
+++ resources/js/filter.js
@@ -6,16 +6,21 @@ $(document).ready(function() {
 		publishers.push(publisher);
 	});
 
+	// loop through each publisher
 	for (var i=0; i<publishers.length; i++) {
 		var selector = $("#writing aside ul#publisher li[data-publisher='"+publishers[i]+"']");
+		
+		// if the publisher already exists, increment the count
 		if (selector.length > 0) {
 			var originalCount = selector.children('span').text() * 1;
 			var newCount = originalCount + 1;
 			selector.children('span').text(newCount);
+			
+		// otherwise, add the publisher to the list
 		} else {
 			$("#writing aside ul#publisher").append('<li data-publisher="'+ publishers[i] +'"><label>'+ publishers[i] +'</label><span>1</span></li>');
 		}
 	}
 	
-	console.log(publishers);
+	$('#articles').quicksand( $('#articles2 article') );
 });
